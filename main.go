97176a63624e4ad3c1cc8f89e867cbb64a686f65
Leonardpepa <leopepp4@gmail.com>
2024-01-03 21:35:01 +0200

    implmentation of Rabin Karp algo

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"string-matching/internal"
+	"math"
 )
 
 func main() {
@@ -31,11 +31,41 @@ Language: English
 `
 
 	pattern := "recently"
-
-	s := internal.BruteForceSearch(input, pattern)
+	s := RabinKarp(input, pattern, 256, 13)
 	fmt.Println(input[s : s+len(pattern)])
+}
 
-	s = internal.BruteForceSearchV2(input, "English")
-	fmt.Println(input[s : s+len(pattern)])
+func RabinKarp(input string, pattern string, d int, q int) int {
+	n := len(input)
+	m := len(pattern)
 
+	h := int(math.Pow(float64(d), float64(m-1))) % q
+
+	p := 0
+	t := 0
+
+	for i := 0; i < m; i++ {
+		p = (d*p + int(pattern[i])) % q
+		t = (d*t + int(input[i])) % q
+	}
+
+	for s := 0; s < n-m; s++ {
+
+		if p == t {
+			if pattern == input[s:s+m] {
+				return s
+			}
+		}
+
+		if s < n-m {
+			t = (d*(t-int(input[s])*h) + int(input[s+m])) % q
+
+			for t < 0 {
+				t = t + q
+			}
+		}
+
+	}
+
+	return -1
 }

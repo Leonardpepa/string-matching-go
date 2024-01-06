522fe56850afc03d5ad001a000a82dc5dcd0a853
Leonardpepa <leopepp4@gmail.com>
2024-01-06 22:59:02 +0200

    added tests for all implementations

--- main_test.go
+++ main_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"string-matching/internal/DFA"
 	"string-matching/internal/bruteForce"
+	"string-matching/internal/rabinKarp"
 	"testing"
 )
 
-func TestBruteForce(t *testing.T) {
-	t.Run("brute force approach", func(t *testing.T) {
-		input := `Project Gutenberg's International Short Stories: French, by Various
+var input = `Project Gutenberg's International Short Stories: French, by Various
 
 This eBook is for the use of anyone anywhere at no cost and with
 almost no restrictions whatsoever.  You may copy it, give it away or
@@ -15,24 +15,14 @@ re-use it under the terms of the Project Gutenberg License included
 with this eBook or online at www.gutenberg.net
 `
 
-		cases := []string{
-			"Gutenberg",
-			"Project",
-			"online at www.gutenberg.net",
-			"restrictions whats",
-			"by Various\n\nThis eBook is for the use of anyone anywhere at no cost and with\nalmost no restrictions whatsoever.  You may copy it, give it away or",
-			": French,",
-			" ",
-		}
-
-		for _, val := range cases {
-			shouldSucceed(t, input, val, bruteForce.MatchString)
-		}
-
-		for _, val := range cases {
-			shouldSucceed(t, input, val, bruteForce.MatchStringV2)
-		}
-	})
+var cases = []string{
+	"Gutenberg",
+	"Project",
+	"online at www.gutenberg.net",
+	"restrictions whats",
+	"by Various\n\nThis eBook is for the use of anyone anywhere at no cost and with\nalmost no restrictions whatsoever.  You may copy it, give it away or",
+	": French,",
+	" ",
 }
 
 func shouldSucceed(t *testing.T, input string, pattern string, matcher func(input string, pattern string) ([]int, error)) {
@@ -47,4 +37,32 @@ func shouldSucceed(t *testing.T, input string, pattern string, matcher func(inpu
 			t.Errorf("expected %s, got %s", pattern, input[index:index+len(pattern)])
 		}
 	}
+}
+
+func TestBruteForce(t *testing.T) {
+	t.Run("brute force approach", func(t *testing.T) {
+		for _, val := range cases {
+			shouldSucceed(t, input, val, bruteForce.MatchString)
+		}
+
+		for _, val := range cases {
+			shouldSucceed(t, input, val, bruteForce.MatchStringV2)
+		}
+	})
+}
+
+func TestRabinKarp(t *testing.T) {
+	t.Run("rabin karp approach", func(t *testing.T) {
+		for _, val := range cases {
+			shouldSucceed(t, input, val, rabinKarp.MatchString)
+		}
+	})
+}
+
+func TestDFA(t *testing.T) {
+	t.Run("DFA approach", func(t *testing.T) {
+		for _, val := range cases {
+			shouldSucceed(t, input, val, DFA.MatchString)
+		}
+	})
 }
